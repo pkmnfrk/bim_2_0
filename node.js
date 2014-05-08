aa99a8075ec97646fb05a7451d0c48550da6f4d7
Mike Caron <mcaron@klick.com>
2014-05-08 12:20:42 -0400

    Clean up the project folder a bit

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 var static = require("node-static");
 
-var file = new static.Server('.');
+var file = new static.Server('./static');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
